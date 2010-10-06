4d8505b92cb5ccd33597942c2c5099f8f5659658
Sergio González <bigmonachus@gmail.com>
2010-10-05 23:02:02 -0500

    compile program

--- ocl.cc
+++ ocl.cc
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <CL/opencl.h>
+
 using namespace std;
 
 //called by ocl when something blows up
@@ -19,6 +22,7 @@ private:
   void _print_profile_info (cl_platform_id pf, string msg,cl_platform_info id);
 public:
   OCL();
+  cl_program createProgram (string fname);
   virtual ~OCL();
 };
 
@@ -104,11 +108,13 @@ OCL::OCL (){
     free (str);
   }
 
+  //TODO: Make it use multiple CPUs as well..
+
   //===========Create context
   const cl_context_properties plist [] = {
     CL_CONTEXT_PLATFORM,
     (cl_context_properties) this->platform,
-    0};
+    0}; //END WITH 0..
   this->context = clCreateContext (plist,
 				   this->num_devices,
 				   this->devices,
@@ -128,7 +134,66 @@ OCL::OCL (){
     exit (-1);
   }
 
+  //   COMMAND QUEUE
+  cl_command_queue q = clCreateCommandQueue (this->context,
+					     this->devices [0], //This is just a test.. use gpu
+					     0,
+					     &err);
+  if (err!=CL_SUCCESS){
+    cout << "Cant create command queue\n";
+    exit (-1);
+  }
+
+  //do something
   
+  //read source
+  cl_program prgrm = createProgram("ocl.cl");
+
+  
+  clReleaseProgram (prgrm);
+
+  if (err!=CL_SUCCESS){
+    cout << " ...\n";
+  }
+  
+  
+  clReleaseCommandQueue (q);
+  //   END COMMAND QUEUE
+}
+
+cl_program OCL::createProgram (string fname){
+  cl_int err;
+  ifstream file ("ocl.cl");
+  stringstream ss;
+  
+  int num_lines = 1;
+  while (file.good ()){
+    char c;
+    if (c=='\n') num_lines++;
+    c = file.get();
+    ss.put (c);
+  }
+  
+  char *src [num_lines];
+  size_t lengths [num_lines];
+  int i=0;
+  
+  while (!ss.eof ()){
+    char *str=(char*)malloc (128*sizeof (char));
+    ss.getline(str,128);  
+    string s = str;
+    lengths [i]=s.length ();
+    
+    src[i]=str;
+    i++;
+  }
+  
+  cl_program prgrm = clCreateProgramWithSource (this->context,
+						num_lines,
+						(const char**) src,
+						lengths,
+						&err);
+  return prgrm;
 }
 
 // Helpers
